3e78602a339bdb2c4abd6c9eae61fe4ca4d07f1b
SteveTaotao <lzt5025879@gmail.com>
2023-01-05 22:36:07 +0800

    update leetcode4

--- LeetCode4_MedianOfTwoSortedArrays/src/main.rs
+++ LeetCode4_MedianOfTwoSortedArrays/src/main.rs
@@ -1,3 +1,23 @@
+struct Solution {}
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let mut a = nums1.clone();
+        let mut b = nums2.clone();
+        a.append(&mut b);
+        a.sort();
+        print!("a = {:?} \n", a);
+        if a.len() % 2 != 0 {
+            a[a.len() / 2] as f64
+        } else {
+            let v1 = (a[a.len() / 2] + a[a.len() / 2 - 1]) as f64;
+            v1 / 2.0
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums1: Vec<i32> = vec![1, 2, 3, 4];
+    let nums2: Vec<i32> = vec![3, 4, 5, 6];
+    let get_median_sorted_arrays = Solution::find_median_sorted_arrays(nums1, nums2);
+    println!("get_median_sorted_arrays {} \n", get_median_sorted_arrays);
 }
